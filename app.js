62c621f60766eda908b919f4bdd0319bab525115
espedito-martins <espedito.martins.dev@gmail.com>
2025-11-05 19:14:21 -0300

    Vingadores Ultimato

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const placarEl = document.getElementById('total-pontos');
 const mensagemVaziaEl = document.querySelector('.mensagem-lista-vazia');
 
 // Adicionei nova mudança
-// Adicionei novo comentario
+// Vingadores Ultimato
 
 let totalPontos = 0;
 function atualizarPlacar() {
